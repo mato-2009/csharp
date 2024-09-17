8f023a646e1541c94b3b2537148167c6e097ee6f
Martin Kovalčík <kovalcikmartin54@gmail.com>
2024-09-17 10:09:17 +0200

    008 cisla

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Ako sa voláš?");
+﻿/*Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno + "\n");
 Console.WriteLine("Koľko máš rokov?");
@@ -6,14 +6,19 @@ string vstup = Console.ReadLine() ?? "0";
 Console.WriteLine("Aké je tvoje obľúbené jedlo?");
 string? jedlo = Console.ReadLine();
 Console.WriteLine("Hmm, " + jedlo + " znie skvele");
-Console.WriteLine("Volám sa "  + meno + " Mám " + vstup + " Moje obľúbené jedlo je " + jedlo);
+Console.WriteLine("Volám sa "  + meno + " Mám " + vstup + " Moje obľúbené jedlo je " + jedlo);*/
 
+int prvecislo = 50;
+int druhecislo = 60;
+Console.WriteLine(prvecislo + druhecislo);
+int tretiecislo = 505484789;
+int stvrtecislo = 458962558;
+Console.WriteLine(tretiecislo - stvrtecislo);
+int piatecislo = 96;
+int siestecislo = 7;
+Console.WriteLine(piatecislo / siestecislo);
 
-
-
-
-
-
-
-
-
+Console.WriteLine(10 == 50);
+Console.WriteLine(50 == 50);
+Console.WriteLine(50 > 60);
+Console.WriteLine(60 < 70);
