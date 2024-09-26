73bba7ce1879faef4d12e35a39af03e68829d571
Martin Kovalčík <kovalcikmartin54@gmail.com>
2024-09-26 08:35:32 +0200

    cvicenie podmienky 26.9 mato

---
+++ Cvicenie_Podmienky/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadajte znak pre matematicku operaciu");
+            string operation = Console.ReadLine();
+            double number1 = 10;
+            double number2 = 5;
+
+
+            if (operation == "+")
+            {
+
+                Console.WriteLine(number1 + number2);
+
+            }
+
+            else if (operation == "-")
+            {
+                Console.WriteLine(number1 - number2);
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(number1 * number2);
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine(number1 / number2);
+
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+} 
+
+
+
+
+
+
+
+
+
