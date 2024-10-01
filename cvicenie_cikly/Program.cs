6749d6d3d8de797b6e146bc50f0a69c0ad7033a4
Martin Kovalčík <kovalcikmartin54@gmail.com>
2024-10-01 11:05:11 +0200

    nedorobene cykli du 1.10

--- cvicenie_cikly/Program.cs
+++ cvicenie_cikly/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /* List<string> listofnames = new List<string>();
+            listofnames.Add("Jakub");
+            listofnames.Add("Marek");
+            listofnames.Add("Jozo");
+            listofnames.Add("Marekdva");
+            listofnames.Add("Jozodva");
+            
+            
+            
+            foreach (string name in listofnames)
+            {
+
+                Console.WriteLine(name);*/
+
+
+
+            /* int postup = 001;
+
+             List<string> listnames = new List<string>();
+
+             listnames.Add("Jozo");
+             listnames.Add("Fero");
+             listnames.Add("Andrej");
+             listnames.Add("Martin");
+             listnames.Add("Jakub");
+             listnames.Add("Karol");
+
+             foreach (string name in listnames)
+             {
+
+                 Console.WriteLine(postup + "." + name);
+                 postup++;
+             }
+             */
+
+            Console.WriteLine("Zadajte Meno");
+
+            string meno = Console.ReadLine();
+
+            Console.WriteLine(meno);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
