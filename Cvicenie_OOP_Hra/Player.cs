96e08d78d88434af4086c1688ce1420151b4353d
Martin Kovalčík <kovalcikmartin54@gmail.com>
2024-10-17 09:19:38 +0200

    cvicenia hra mana

--- Cvicenie_OOP_Hra/Player.cs
+++ Cvicenie_OOP_Hra/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +12,76 @@ namespace Cvicenie_OOP_Hra
     {
 
         public string Name { get; set; }
-        public  int AttackPower { get; set; }
+        public int AttackPower { get; set; }
 
         public int HP { get; set; }
         public int Mana { get; set; }
+        public int CritChance { get; set; }
 
-        public Player(string name, int attackPower, int hP, int mana)
+        public Player(string name, int attackPower, int hP, int mana, int critChance)
         {
             Name = name;
             AttackPower = attackPower;
             HP = hP;
             Mana = mana;
+            CritChance = critChance;
         }
-    
-    public void DamagePlayer(Player player)
+
+        public void DamagePlayer(Player player)
         {
-         var HPofEnemy = player.HP;
-            int AttackofCurrentPlayer = this.AttackPower;
-            int HPofEnemyAfterFight = HPofEnemy - AttackofCurrentPlayer;
-            player.HP = HPofEnemyAfterFight;
-        
+            int damageMultiplier = 1;
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.CritChance)
+            {
+                damageMultiplier = 2;
+                Console.WriteLine(this.Name + " dal kriticky zasah! ");
+
+                int HPofEnemy = player.HP;
+                int AttackofCurrentPlayer = this.AttackPower * damageMultiplier;
+                int HPofEnemyAfterFight = HPofEnemy - AttackofCurrentPlayer;
+                player.HP = HPofEnemyAfterFight;
+
+            }
+
         }
-    
-    
-    
+        public bool Heal()
+        {
+            if (this.Mana > 0)
+            {
+
+                int HPOfPlayer = this.HP;
+                int ManaOfPlayer = this.Mana;
+                int HPOfPlayerAfterHeal = HPOfPlayer + ManaOfPlayer;
+                this.Mana = 0;
+                this.HP = HPOfPlayerAfterHeal;
+                return true;
+            }
+
+            else
+            {
+
+                return false;
+            }
+
+        }
+
+        public void RefilMana(int newMana)
+        {
+            this.Mana += newMana;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
--- Cvicenie_OOP_Hra/Program.cs
+++ Cvicenie_OOP_Hra/Program.cs
@@ -1,57 +1,106 @@
-﻿namespace Cvicenie_OOP_Hra
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Cvicenie_OOP_Hra
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Player KladnaPostava = new Player("Knight", 10, 100, 50);
+            Player KladnaPostava = new Player("Knight", 15, 100, 50, 50);
 
-            Player ZapornaPostava = new Player("MegaKnight", 10, 100, 50);
-
-
-            Console.WriteLine("Zaporna postava HP:" + ZapornaPostava.HP);
-            KladnaPostava.DamagePlayer(ZapornaPostava);
-            Console.WriteLine("Zaporna postava HP:" + ZapornaPostava.HP);
-
+            Player ZapornaPostava = new Player("MegaKnight", 5, 300, 50, 50);
 
             while (true)
             {
-            if (ZapornaPostava.HP <= 0) 
-            {
+                Console.WriteLine("Kladna postava HP:" + KladnaPostava.HP);
+                Console.WriteLine("Zaporna postava HP:" + ZapornaPostava.HP);
+                KladnaPostava.DamagePlayer(ZapornaPostava);
+                ZapornaPostava.DamagePlayer(KladnaPostava);
+                //Pokus o ozdravenie kladnej postavy a nasledny vipis ci sa to podarilo//
+                if (KladnaPostava.HP <= 20)
+                {
+                    bool wasHealed = KladnaPostava.Heal();
+                    if (wasHealed)
+                    {
 
+                        Console.WriteLine("Kladna postava bola ozdravena");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kladna uz nema manu a nebola ozdravena");
+                    }
+
+
+
+
+                }
+                //Pokus o ozdravenie zapornej postavy a nasledny vipis ci sa to podarilo// 
+                if (ZapornaPostava.HP <= 20)
+                {
+                    bool wasHealed = ZapornaPostava.Heal();
+                    if (wasHealed)
+                    {
+
+                        Console.WriteLine("Zaporna postava bola ozdravena");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Zaporna postava nema manu a nebola ozdravena");
+
+                    }
+
+                }
+
+                Random random = new Random();
+                int randomNumber = random.Next(0, 50);
+                if (randomNumber <= 5)
+                {
+                    KladnaPostava.RefilMana(10);
+                    Console.WriteLine("---Kladna postava dostala dar od Boha a doplnila sa jej mana---");
+                }
+
+              
+                int randomNumber2 = random.Next(0, 50);
+                if (randomNumber2 <= 5)
+                {
+                    ZapornaPostava.RefilMana(10);
+                    Console.WriteLine("---Zaporna postava dostala dar od Boha a doplnila sa jej mana---");
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+                if (ZapornaPostava.HP <= 0)
+                {
                     Console.WriteLine("kladna postava vyhrala");
                     break;
-                
+
                 }
 
-            if(KladnaPostava.HP <= 0)
+                if (KladnaPostava.HP <= 0)
                 {
                     Console.WriteLine("Zaporna postava prehrala");
+                    break;
+
                 }
-                break; 
+
+
+
 
             }
-            
-            
-            
-                           
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         }
     }
-} 
+}
 
 
 
