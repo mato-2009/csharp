5a83c2f9a5e6b434de1360f0067376011f793511
Martin Kovalčík <kovalcikmartin54@gmail.com>
2025-01-30 09:08:18 +0100

    tictactoe

---
+++ WPF_Piskorky/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Piskorky
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public char LastPlayer { get; set; } = 'O';
+
+        public char PlayerOne { get; set; } = '✕';
+
+        public char PlayerTwo { get; set; } = 'O';
+       
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ProcessClick(Button button)
+        {
+            var buttonContent = button.Content.ToString();
+            if (buttonContent.Contains(PlayerOne) || buttonContent.Contains(PlayerTwo))
+            {
+                return;
+            }
+
+
+            if (LastPlayer == PlayerOne)
+            {
+                button.Content = PlayerTwo;
+                LastPlayer = PlayerTwo;
+
+            }
+
+            else if (LastPlayer == PlayerTwo)
+            {
+                button.Content = PlayerOne;
+                LastPlayer = PlayerOne;
+
+            }
+            var isWinner = CheckWinner();
+            if (isWinner)
+            {
+                MessageBox.Show("Vyhral hráč:" + LastPlayer);
+            }
+        }
+        private void Button_0x0_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessClick(sender as Button);
+        }
+
+        public bool CheckWinner()
+        {
+            var b_0x0 = Button_0x0.Content.ToString();
+            var b_0x1 = Button_0x1.Content.ToString();
+            var b_0x2 = Button_0x2.Content.ToString();
+            var b_1x0 = Button_1x0.Content.ToString();
+            var b_1x1 = Button_1x1.Content.ToString();
+            var b_1x2 = Button_1x2.Content.ToString();
+            var b_2x0 = Button_2x0.Content.ToString();
+            var b_2x1 = Button_2x1.Content.ToString();
+            var b_2x2 = Button_0x0.Content.ToString();
+
+            if (b_0x0 == b_0x1 && b_0x0 == b_0x2)
+            {
+                return true;
+
+            }
+            if (b_1x0 == b_1x1 && b_1x0 == b_1x2)
+            {
+                return true;
+            }
+            if (b_2x0 == b_2x1 && b_2x0 == b_2x2)
+            { return true; }
+            if (b_0x0 == b_0x1 && b_0x0 == b_0x2)
+            { return true; }
+            if (b_0x1 == b_1x1 && b_0x1 == b_2x1)
+            { return true; }
+            if (b_0x2 == b_1x2 && b_0x2 == b_2x2)
+            { return true; }
+
+        }
+
+
+        }
+    }
+
+
+
+
--- WPF_TestApplication/MainWindow.xaml.cs
+++ WPF_TestApplication/MainWindow.xaml.cs
@@ -16,60 +16,87 @@ namespace WPF_TestApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<string> AllUsers { get; set; } = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+
             Button_ShowName.IsEnabled = false;
 
-            ComboBox_AccountRole.Itmes.Add("Student");
-            ComboBox_AccountRole.Itmes.Add("Student");
+            ComboBox_AccountRole.Items.Add("Teacher");
+            ComboBox_AccountRole.Items.Add("Student");
 
-
-
-
+            AllUsers.Add("Teacher Michal");
+            AllUsers.Add("Teacher Igor");
+            AllUsers.Add("student Marek");
+            AllUsers.Add("student Alica");
+            AllUsers.Add("student Andreas");
         }
 
-        private void button_Click_ShowName(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e)
         {
-            var inputName = Textbox_InputFirstName.Text;
-            
+            //var accountrole = ComboBox_AccountRole.SelectedItem as string;
+            //var inputName = TextBox_InputFirstName.Text;
+            //Label_FirstName.Content = accountrole + inputName;
+
+            var newUser = (ComboBox_AccountRole.SelectedItem as string) + " " + TextBox_InputFirstName.Text;
+            Label_FirstName.Content = newUser;
+            AllUsers.Add(newUser);
+
+            ListBox_AllUsers.Items.Clear();
+            foreach (var item in AllUsers)
+            {
+                ListBox_AllUsers.Items.Add(item);
+            }
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextBox_InputFirstName_TextChanged_1(object sender, TextChangedEventArgs e)
         {
-            var textBox = sender as TextBox;
-            var inputName = textBox.Text;
-            Label_FirstName.Content = inputName;
+            /*var textBox = sender as TextBox;
+            var inputName = TextBox_InputFirstName.Text;
+            Label_FirstName.Content = inputName;*/
         }
 
         private void CheckBox_AcceptRules_Checked(object sender, RoutedEventArgs e)
         {
-            bool acceptRules = CheckBox_AcceptRules.Value;
-            if (acceptRules)
-            {
-                button.IsEnabled = true;
-
-            }
-            else
-            {
-                button.IsEnabled = false;
-            }
+            //Button_ShowName.IsEnabled = true;
         }
 
+        private void CheckBox_AcceptRules_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //Button_ShowName.IsEnabled = false;
 
-
-
-
-
-
+        }
 
         private void CheckBox_AcceptRules_Click(object sender, RoutedEventArgs e)
         {
-         Button
+            Button_ShowName.IsEnabled = CheckBox_AcceptRules.IsChecked.Value;
+
+            /* bool acceptedRules = CheckBox_AcceptRules.IsChecked.Value;
+             if (acceptedRules)
+             {
+                 Button_ShowName.IsEnabled = true;
+             }
+             else
+             {
+                 Button_ShowName.IsEnabled = false;
+             }*/
         }
 
+        private void ChcekBox_DoNotAccpetRules_Checked(object sender, RoutedEventArgs e)
+        {
+            Button_ShowName.IsEnabled = CheckBox_AcceptRules.IsChecked.Value;
+        }
 
-
-
-
-
+        private void Button_FilterOnlyTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var user in AllUsers)
+            {
+                if (user.Contains("Teacher"))
+                {
+                    ListBox_AllUsers.Items.Add(user);
+                }
+            }
+        }
+    }
+}
